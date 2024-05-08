b87a0703137f43d37ba32a21b5b04ccd45505ccd
forfd8960 <forfd8960@gmail.com>
2024-05-08 08:25:51 +0800

    Optimize process_dedup with Vec retain method

--- csvutils/src/process/dedup.rs
+++ csvutils/src/process/dedup.rs
@@ -2,30 +2,18 @@ use std::collections::HashSet;
 
 use anyhow::anyhow;
 
-use super::read_csv;
+use crate::process::read_csv;
 
 pub fn process_dedup(input: &str, field: &str) -> anyhow::Result<Vec<Vec<String>>> {
-    let csv_result = read_csv(input)?;
+    let mut csv_result = read_csv(input)?;
 
-    let mut field_idx: usize = 0;
     let pos = csv_result.headers.iter().position(|x| x == field);
     match pos {
-        Some(idx) => field_idx = idx,
+        Some(idx) => {
+            let mut data_set = HashSet::new();
+            csv_result.data.retain(|r| data_set.insert(r[idx].clone()));
+            anyhow::Ok(csv_result.data)
+        }
         None => return Err(anyhow!("field not found")),
     }
-
-    let mut dedup_data: Vec<Vec<String>> = Vec::with_capacity(csv_result.data.len() as usize);
-    let mut data_set = HashSet::new();
-    for record in csv_result.data {
-        let r = record.clone();
-        let v = r[field_idx].clone();
-        if data_set.contains(&v) {
-            continue;
-        }
-
-        data_set.insert(v);
-        dedup_data.push(record);
-    }
-
-    anyhow::Ok(dedup_data)
 }
