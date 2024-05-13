f991531fead7b33277712897e24de661cf08601a
forfd8960 <forfd8960@gmail.com>
2024-05-14 07:17:07 +0800

    fix: fix process_filter and add more details in README

--- csvutils/README.md
+++ csvutils/README.md
@@ -1,10 +1,32 @@
 # csvutils
 
-## Receive Multiple values
+## Merge
 
 ```sh
-cargo run -- merge --inputs csv1 --inputs csv2 --output csv
+~/D/r/c/csvutils> cargo run -- merge --inputs test.csv --inputs test1.csv --output merge.csv
+   Compiling csvutils v0.1.0 (~/Documents/rust-code/csvutil/csvutils)
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 2.74s
+     Running `target/debug/csvutils merge --inputs test.csv --inputs test1.csv --output merge.csv`
+Options { cmd: Merge(MergeOpts { inputs: ["test.csv", "test1.csv"], output: "merge.csv" }) }
+MergeOpts { inputs: ["test.csv", "test1.csv"], output: "merge.csv" }
+StringRecord(["f1", "f2", "f3"])
+StringRecord(["f1", "f2", "f3"])
+process_merge: ()
+```
 
-Options { cmd: Merge(MergeOpts { inputs: ["csv1", "csv2"], output: Some("csv3") }) }
-MergeOpts { inputs: ["csv1", "csv2"], output: Some("csv3") }
+## Filter
+
+```sh
+~/D/r/c/csvutils>
+cargo run -- filter --input merge.csv --output filter.csv --exps f1=111 --exps f2=e
+   Compiling csvutils v0.1.0 (~/Documents/rust-code/csvutil/csvutils)
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 1.50s
+     Running `target/debug/csvutils filter --input merge.csv --output filter.csv --exps f1=111 --exps f2=e`
+
+Options { cmd: Filter(FilterOpts { input: "merge.csv", output: "filter.csv", exps: ["f1=111", "f2=e"] }) }
+FilterOpts { input: "merge.csv", output: "filter.csv", exps: ["f1=111", "f2=e"] }
+fields: {"f2": "e", "f1": "111"}
+StringRecord(["f1", "f2", "f3"])
+idx_value: {1: "e", 0: "111"}
+process_filter: ()
 ```
--- csvutils/src/process/filter.rs
+++ csvutils/src/process/filter.rs
@@ -9,29 +9,39 @@ pub fn process_filter(input_csv: String, output: String, exps: Vec<String>) -> a
         return Err(anyhow!("empty exps"));
     }
 
-    let fieds = parse_exps(exps)?;
+    // parse exps and build column to exp value map
+    let cols = parse_exps(exps)?;
+    println!("fields: {:?}", cols); // {"c1": v1, "c2": "v2"}
 
+    // idx_value contains the index to the  in the csv
     let mut idx_value = HashMap::new();
 
+    // build idx to exp_value mapping from csv headers
     let csv_data = read_csv(&input_csv)?;
     for (idx, hdr) in csv_data.headers.iter().enumerate() {
         let key = hdr.as_str();
-        if !fieds.contains_key(key) {
+        if !cols.contains_key(key) {
             continue;
         }
 
-        idx_value.insert(idx, fieds.get(key).unwrap());
+        idx_value.insert(idx, cols.get(key).unwrap());
     }
+    println!("idx_value: {:?}", idx_value); // {1: "val1", 0: "val2"}
 
     let mut data: Vec<Vec<String>> = Vec::with_capacity(csv_data.data.len());
+
+    // iter over the csv data and for each record in the data
+    // * if one of the idx(col) in the record(r) contains the val, then put the record in the final data
+    // * if not skip the record(r)
     for r in csv_data.data {
         let mut contains_data = false;
 
         for (idx, d) in r.iter().enumerate() {
-            let v = idx_value.get(&idx).unwrap();
-            if d.contains(*v) {
-                contains_data = true;
-            }
+            if let Some(exp_val) = idx_value.get(&idx) {
+                if d.contains(*exp_val) {
+                    contains_data = true;
+                }
+            };
         }
         if contains_data {
             data.push(r);
@@ -44,7 +54,7 @@ pub fn process_filter(input_csv: String, output: String, exps: Vec<String>) -> a
 }
 
 fn parse_exps(exps: Vec<String>) -> anyhow::Result<HashMap<String, String>> {
-    let mut fieds = HashMap::new();
+    let mut cols = HashMap::new();
     for e in exps {
         let field_value: Vec<String> = e.split('=').map(|v| v.to_string()).collect();
 
@@ -52,8 +62,8 @@ fn parse_exps(exps: Vec<String>) -> anyhow::Result<HashMap<String, String>> {
             return Err(anyhow!("bad exp: {}", e));
         }
 
-        fieds.insert(field_value[0].clone(), field_value[1].clone());
+        cols.insert(field_value[0].clone(), field_value[1].clone());
     }
 
-    anyhow::Ok(fieds)
+    anyhow::Ok(cols)
 }
